3dc49fa1069bd438eee1732552f85eec3486f3d0
Victor Ojong <victorojong78@gmail.com>
2023-12-28 17:23:52 +0100

    Feat: added contact form handler

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,53 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+      const userName = document.getElementById("name");
+      const userEmail = document.getElementById("email");
+      const userPhone = document.getElementById("phone");
+      const message = document.getElementById("message");
+
+
+          const setAllFieldsToEmpty = () => {
+              userName.value =
+              userEmail.value =
+              userPhone.value =
+              message.value =
+              "";
+          }
+      const submitBTN = document.getElementById("submitB");
+
+      // email reciever sender function
+      (function () {
+        emailjs.init("yf1anzwTZWgDaYT_e");
+      })();
+
+      submitBTN.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        const email = userEmail.value;
+        const phone = userPhone.value;
+        const messagE = message.value;
+        const name = `${userName.value} : ${phone}`;
+        if (!email || !phone || !message) {
+          return alert("Sorry some required fields are empty");
+        }
+
+        // Prepare the email parameters
+        let params = {
+          from_name: name,
+          from_email: email,
+          message: messagE,
+        };
+
+        // Send the email
+        emailjs.send("service_bs1750k", "template_0isr23u", params).then(
+          function (response) {
+            // sweet allert and set all fields to empty
+            Swal.fire("Sent ", "I will reply your Message ASAP!", "success");
+            setAllFieldsToEmpty()
+          
+          },
+          function (error) {
+            Swal.fire("Failed", "something went wrong", "error");
+           setAllFieldsToEmpty()
+          }
+        );
+      });
